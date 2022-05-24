091aba859147d4dffbec525d947f89c81b77735a
Kasuni <it19028156@my.sliit.lk>
2022-05-24 23:54:09 +0530

    data_preprocessing modified

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -21,7 +21,7 @@ import seaborn as sns
 
 df = pd.read_csv("movie_metadata.csv")
 
-#Select only important attributes for the dataframe for predict the gross of the movie
+# Select only important attributes for the dataframe for predict the gross of the movie
 df = df[["num_critic_for_reviews", "duration", "director_facebook_likes", "actor_3_facebook_likes","actor_1_facebook_likes" ,"num_voted_users","cast_total_facebook_likes",
          "facenumber_in_poster", "num_user_for_reviews", "language", "country", "budget", "actor_2_facebook_likes", "imdb_score","aspect_ratio", "movie_facebook_likes", "gross"]]
 df.head(5)
@@ -33,30 +33,31 @@ print('\nUnique values : \n',df.nunique())
 print('Number of null values of each column')
 df.isna().sum()
 
-#Drop all the null values of country and language columns
+# Drop all the null values of country and language columns
 df = df.dropna(subset=['country','language'],inplace=False)
 
-#Encode the country values to a numberical value
+# Encode the country values to a numberical value
 country = LabelEncoder() #define the encoder
 df['country'] = country.fit_transform(df['country'])
 df["country"].unique()
 
-#Encode the language values to a numerical value
+# Encode the language values to a numerical value
 language = LabelEncoder()  #define the enocoder
 df['language'] = language.fit_transform(df['language'])
 df["language"].unique()
 
 ### Find out most Important Features ####
-#Plotting the Correlation between the numerical values of the Dataset
+# Plotting the Correlation between the numerical values of the Dataset
 correlations = df.corr()
 f,ax = plt.subplots(figsize=(15,15))
 sns.heatmap(correlations, annot=True, cmap="YlGnBu", linewidths=.5)
 
-#According to the heatmap result and nature of the predicted variable , drop other variables from the dataframe while keeping other important variables as independent variables
+# According to the heatmap result and nature of the predicted variable , drop other variables from the dataframe while keeping other important variables as independent variables
 df = df.drop(['actor_3_facebook_likes','cast_total_facebook_likes','facenumber_in_poster','language','aspect_ratio'], axis = 1)
 
 ### Pre Processing
 
+## Method for remove the reocrds of countries which are under a given limit
 def cutoff_countries(countries, limit):
    country_map = {}
    for i in range(len(countries)):
@@ -65,7 +66,8 @@ def cutoff_countries(countries, limit):
        else:
            country_map[countries.index[i]] = countries.index[i]
    return country_map
-       
+
+# Remove countries which are having records of 80       
 country_map = cutoff_countries(df.country.value_counts(), 80)
 df['country'] = df['country'].map(country_map)
 df.country.value_counts()
@@ -83,3 +85,5 @@ df['actor_1_facebook_likes'] = df['actor_1_facebook_likes'].fillna(df['actor_1_f
 df['actor_2_facebook_likes'] = df['actor_2_facebook_likes'].fillna(df['actor_2_facebook_likes'].mean(), inplace=False)
 df['duration'] = df['duration'].fillna(df['duration'].mean(), inplace=False)
 df['director_facebook_likes'] = df['director_facebook_likes'].fillna(df['director_facebook_likes'].mean(), inplace=False)
+
+df.head()
